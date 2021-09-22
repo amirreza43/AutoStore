187bff8301206ccfc9a849a43b8c6028532abfa4
LTIUser <LTIUser@INFEDL31176.lntinfotech.com>
2021-09-22 12:44:24 -0400

    Car class created

---
+++ model/Car.cs
@@ -0,0 +1,15 @@
+namespace AutoStore
+{
+    public class Car{
+        public int Mpg;
+        public int Cylinders;
+        public int Displacement;
+        public int Horsepower;
+        public int Weight;
+        public int Acceleration;
+        public int ModelYear;
+        public int Origin;
+        public string CarName;
+        public decimal Price;
+    }
+}
